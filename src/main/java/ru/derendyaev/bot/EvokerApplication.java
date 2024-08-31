60c307b796151b9062ff83539f9788fc80f67cb7
Dmitrii Derendyaev <dds_stalker@mail.ru>
2024-08-31 21:42:35 +0400

    init evoker bot

---
+++ src/main/java/ru/derendyaev/bot/EvokerApplication.java
@@ -0,0 +1,13 @@
+package ru.derendyaev.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvokerApplication.class, args);
+	}
+
+}
